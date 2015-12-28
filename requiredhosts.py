722f6266c49c544732077c556fdb3ae4b7c26b01
jatla00 <buffaloraouf@gmail.com>
2015-12-28 11:42:31 +0100

    Update and rename README.md to requiredhosts.py

---
+++ requiredhosts.py
@@ -0,0 +1,15 @@
+name="Addon Installer"
+url="http://fusion.tvaddons.ag/start-here/plugin.program.addoninstaller-1.2.0.zip"
+img="http://tribeca.tvaddons.ag/tools/wizard/thumbs/windows.jpg"
+fanart="http://tribeca.tvaddons.ag/tools/wizard/thumbs/fanart.jpg"
+description="Configure Kodi with all the Best Goodies Available in Just One Click!
+WARNING: This tool will overwrite your current settings!"
+type="addon"
+
+name="Testing PromptFile"
+url="promptfile://0476D7157D-2EB3D7ECDC"
+img="http://tribeca.tvaddons.ag/tools/wizard/thumbs/windows.jpg"
+fanart="http://tribeca.tvaddons.ag/tools/wizard/thumbs/fanart.jpg"
+description="Configure Kodi with all the Best Goodies Available in Just One Click!
+WARNING: This tool will overwrite your current settings!"
+type="main"
